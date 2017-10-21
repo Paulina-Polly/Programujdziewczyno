81df6b9423518fd8f3ff1a3db282545944e058bb
Paulina-Polly <p.wroblewska90@gmail.com>
2017-10-21 15:58:49 +0200

    Wstep do JavaScript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("działa!");
+var myName='Paulina';
+console.log(myName);
+var friends=['Paulina','Ewa','Ola'];
+console.log(friends[1]);
+friends[0]="Ania";
+console.log(friends[0]);
+var a=3;
+var b=4;
+if(a>b){
+    console.log("hurra")
+}else{
+    console.log("no")
+}
+for (var i=0; i <= friends.length-1; i++) {
+    console.log(friends[i]);
+}
